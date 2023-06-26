ce0968f3ca27c3033e6fb8d401cc8484c0b091cc
Welldan Yogia <welldanyogia01@gmail.com>
2023-06-27 06:21:10 +0700

    Updated

--- config/configFile.js
+++ config/configFile.js
@@ -15,12 +15,12 @@ exports.info = {
   grup: "",
   apikeyRemoveBg: "vfJDby4EsFbu2M815YHm6Q9p",
   //Digiflazz
+  endpointDigi : 'https://api.digiflazz.com/v1',
   apiKey : '0a0846e7-a2c4-5547-88ec-07b0449d3cee',
   username : 'mohoveg4ML6o',
   //APIGAMES
   merchantId : "M230205PCUY8759RP",
   secretKey :"65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
-  endpointDigi : 'https://api.digiflazz.com/v1',
   endpointAPIgames : 'https://v1.apigames.id/merchant',
 
 }
