494ddf7f1faa4d61f3d97704441b45e1a8191087
Welldan Yogia <63446127+welldanyogia@users.noreply.github.com>
2023-07-02 07:18:09 +0700

    Update configFile.js

--- config/configFile.js
+++ config/configFile.js
@@ -13,14 +13,14 @@ exports.info = {
     bigName: "𝕎𝔼𝔹ℝ𝔸ℕ𝔸"
   },
   grup: "",
-  apikeyRemoveBg: "vfJDby4EsFbu2M815YHm6Q9p",
+  apikeyRemoveBg: "",
   //Digiflazz
   endpointDigi : 'https://api.digiflazz.com/v1',
-  apiKey : '0a0846e7-a2c4-5547-88ec-07b0449d3cee',
-  username : 'mohoveg4ML6o',
+  apiKey : '',
+  username : '',
   //APIGAMES
-  merchantId : "M230205PCUY8759RP",
-  secretKey :"65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
+  merchantId : "",
+  secretKey :"",
   endpointAPIgames : 'https://v1.apigames.id/merchant',
 
 }
