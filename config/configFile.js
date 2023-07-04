d29e4782b49a09276de0969cce20f045b20f0236
Welldan Yogia <welldanyogia01@gmail.com>
2023-07-05 03:10:14 +0700

    update cek koneksi

--- config/configFile.js
+++ config/configFile.js
@@ -16,14 +16,14 @@ exports.info = {
   //NAMA BOT
   BotName : "Webrana Bot",
   grup: "",
-  apikeyRemoveBg: "",
+  apikeyRemoveBg: "vfJDby4EsFbu2M815YHm6Q9p",
   //Digiflazz
   endpointDigi : 'https://api.digiflazz.com/v1',
-  apiKey : '',
-  username : '',
+  apiKey : '0a0846e7-a2c4-5547-88ec-07b0449d3cee',
+  username : 'mohoveg4ML6o',
   //APIGAMES
-  merchantId : "",
-  secretKey :"",
+  merchantId : "M230205PCUY8759RP",
+  secretKey :"65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
   endpointAPIgames : 'https://v1.apigames.id/merchant',
 
 }
