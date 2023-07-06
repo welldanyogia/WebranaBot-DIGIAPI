bed52ebbdc8797de3cf5fb0d8519b31d627e61a3
Welldan Yogia <welldanyogia01@gmail.com>
2023-07-06 21:02:58 +0700

    update cekkoneksi

--- config/configFile.js
+++ config/configFile.js
@@ -1,29 +1,28 @@
 exports.info = {
-  prefix: '.',
-  prefix2: '-',
-  owner:     {
+  prefix: ".",
+  prefix2: "-",
+  owner: {
     name: "Welldan Yogia",
-    number: ["6281252878597@s.whatsapp.net","6282131089084@s.whatsapp.net"],
-    numero: ["6281252878597@s.whatsapp.net","6282131089084@s.whatsapp.net"]//change to your owner number
-    },
+    number: ["6281252878597@s.whatsapp.net", "6282131089084@s.whatsapp.net"],
+    numero: ["6281252878597@s.whatsapp.net", "6282131089084@s.whatsapp.net"], //change to your owner number
+  },
   bot: {
     name: "WEBRANA BOT",
     number: "6285648622105@s.whatsapp.net", //change to your bot number
     shortName: "Webrana",
-    bigName: "𝕎𝔼𝔹ℝ𝔸ℕ𝔸"
+    bigName: "𝕎𝔼𝔹ℝ𝔸ℕ𝔸",
   },
 
   //NAMA BOT
-  BotName : "Webrana Bot",
+  BotName: "Webrana Bot",
   grup: "",
   apikeyRemoveBg: "vfJDby4EsFbu2M815YHm6Q9p",
   //Digiflazz
-  endpointDigi : 'https://api.digiflazz.com/v1',
-  apiKey : '0a0846e7-a2c4-5547-88ec-07b0449d3cee',
-  username : 'mohoveg4ML6o',
+  endpointDigi: "https://api.digiflazz.com/v1",
+  apiKey: "0a0846e7-a2c4-5547-88ec-07b0449d3cee",
+  username: "mohoveg4ML6o",
   //APIGAMES
-  merchantId : "M230205PCUY8759RP",
-  secretKey :"65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
-  endpointAPIgames : 'https://v1.apigames.id/merchant',
-
-}
+  merchantId: "M230205PCUY8759RP",
+  secretKey: "65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
+  endpointAPIgames: "https://v1.apigames.id/merchant",
+};
