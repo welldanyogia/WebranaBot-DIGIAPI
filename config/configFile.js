53c9bdd9a83fd8d90caeb77751deaf4d3087b466
welldanyogia <welldanyogia01@gmail.com>
2024-08-02 18:15:25 +0700

    update addlist,deletelist,addowner,deleteowner, update qris,checkip

--- config/configFile.js
+++ config/configFile.js
@@ -1,28 +1,24 @@
-exports.info = {
-  prefix: ".",
-  prefix2: "-",
-  owner: {
-    name: "Welldan Yogia",
-    number: "6285648622105@s.whatsapp.net",
-    numero: "6285648622105@s.whatsapp.net",
-  },
-  bot: {
-    name: "WEBRANA BOT",
-    number: "6282131670100@s.whatsapp.net", //change to your bot number
-    shortName: "Webrana",
-    bigName: "𝕎𝔼𝔹ℝ𝔸ℕ𝔸",
-  },
 
-  //NAMA BOT
-  BotName: "Webrana Bot",
-  grup: "",
-  apikeyRemoveBg: "vfJDby4EsFbu2M815YHm6Q9p",
-  //Digiflazz
-  endpointDigi: "https://api.digiflazz.com/v1",
-  apiKey: "0a0846e7-a2c4-5547-88ec-07b0449d3cee",
-  username: "mohoveg4ML6o",
-  //APIGAMES
-  merchantId: "M230205PCUY8759RP",
-  secretKey: "65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
-  endpointAPIgames: "https://v1.apigames.id/merchant",
+exports.info = {
+    prefix: ".",
+    prefix2: "-",
+    owner: [
+        { name: "Welldan Yogia", number: "6285648622105@s.whatsapp.net", numero: "6285648622105@s.whatsapp.net" },
+    { name: "Welldan Yogia", number: "6282131670100@s.whatsapp.net", numero: "6282131670100@s.whatsapp.net" }
+    ],
+    bot: {
+        name: "WEBRANA BOT",
+        number: "6287886296587@s.whatsapp.net",
+        shortName: "Webrana",
+        bigName: "𝕎𝔼𝔹ℝ𝔸ℕ𝔸",
+    },
+    BotName: "Webrana Bot",
+    grup: "",
+    apikeyRemoveBg: "vfJDby4EsFbu2M815YHm6Q9p",
+    endpointDigi: "https://api.digiflazz.com/v1",
+    apiKey: "0a0846e7-a2c4-5547-88ec-07b0449d3cee",
+    username: "mohoveg4ML6o",
+    merchantId: "M230205PCUY8759RP",
+    secretKey: "65c37d60b2a8ca3e5d9bdbe5b0c7f9fe10aa7d0149a92bd618cacaf03daa3933",
+    endpointAPIgames: "https://v1.apigames.id/merchant",
 };
